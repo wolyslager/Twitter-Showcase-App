dff54f2a99c5a9e0ea58a747e982d7fd72bbe488
wolyslager <olyslager.willy@gmail.com>
2020-11-16 11:11:55 -0600

    added styling

--- src/Components/InfoBoxes.js
+++ src/Components/InfoBoxes.js
@@ -9,6 +9,7 @@ const InfoBoxes = () => {
 			<Card className='card' style={{ width: '18rem', height: '18rem' }} bg="primary" text="light">
 			  <Card.Body>
 			    <Card.Title>What is it?</Card.Title>
+			    <hr className="horizontal"></hr>
 			    <Card.Text>
 			      This application lets you search for yout favorite Twitter users and see their most recent Tweets! It also
 			      allows you to view a random selection of Tweets if you want. 
@@ -22,6 +23,7 @@ const InfoBoxes = () => {
 			<Card className='card' style={{ width: '18rem', height: '18rem' }} bg="primary" text="light">
 			  <Card.Body>
 			    <Card.Title>How Do I Use It?</Card.Title>
+			    <hr className="horizontal"></hr>
 			    <Card.Text>
 			      Click on the "Find Tweets" tab and type in a Twitter user into the search bar.
 			       If you're feeling lucky, click on the "Roll The Dice" tab!
@@ -35,6 +37,7 @@ const InfoBoxes = () => {
 			<Card className='card' style={{ width: '18rem', height: '18rem' }} bg="primary" text="light">
 			  <Card.Body>
 			    <Card.Title>How Does It Work?</Card.Title>
+			    <hr className="horizontal"></hr>
 			    <Card.Text>
 			      The Twitter API App has a ReactJS front end that communicates with a NodeJS server. The server 
 			      makes requests to the Twitter API and the response is then rendered here!
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,13 +4,18 @@ import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitterSquare } from '@fortawesome/free-brands-svg-icons'
+
 import './SearchBar.css'
 
 const NavbarComp = (props) => {
 	return(
 		<div>
 		  <Navbar bg="primary" variant="dark">
-			    <Navbar.Brand href="#home">Twitter API App</Navbar.Brand>
+			    <Navbar.Brand href="#home">
+			    	<FontAwesomeIcon icon={faTwitterSquare} className="fa-lg"onClick={() => props.changeTabs(1)} />
+			    </Navbar.Brand>
 			    <Nav className="mr-auto">
 			      <Nav.Link onClick={() => props.changeTabs(1)}>Home</Nav.Link>
 			      <Nav.Link onClick={() => props.changeTabs(2)}>Find Tweets</Nav.Link>
